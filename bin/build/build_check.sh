716f204f34f6aebe0347c507203680ee5c4cd61a
0xcasuwu <ed995499@gmail.com>
2025-05-14 13:37:48 +0000

    Add minimal-contract and setup oyl-sdk globally

--- bin/build/build_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-$HOME/.cargo/bin/cargo build
-echo "Build exit code: $?"
-
-# Check for WebAssembly output
-echo "Looking for WebAssembly files:"
-find ./target -name "*.wasm" | grep -v target/debug || echo "No WebAssembly files found"
